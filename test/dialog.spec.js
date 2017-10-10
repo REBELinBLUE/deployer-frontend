2fe5e86b1d308aa08d9d2c27ba8e79be9fb8044b
Stephen Ball <stephen@rebelinblue.com>
2017-10-10 09:39:33 +0100

    Fix broken test

--- test/dialog.spec.js
+++ test/dialog.spec.js
@@ -8,15 +8,13 @@ describe('Dialog Helpers', () => {
   beforeEach(() => {
     document.body.innerHTML = `
       <div class="modal fade" id="group">
-        
+
       </div>
     `;
   });
 
   it('has document', () => {
-    var button = document.querySelector('button.btn');
-
-    console.log(button);
-    expect(button.nodeName).eql('BUTTON');
+    var button = document.querySelector('.modal');
+    expect(button.nodeName).to.be.equal('DIV');
   });
 });
