482c24a3d17474a562a4a0b6107340697ec3873d
Stephen Ball <stephen@rebelinblue.com>
2017-11-30 22:03:14 +0000

    Work on user levels

--- src/models/User.js
+++ src/models/User.js
@@ -2,11 +2,32 @@ import Backbone from 'backbone';
 
 import routes from '../routes';
 
+export const ADMIN = 1;
+export const MANAGER = 2;
+export const COLLABORATOR = 2;
+export const VIEWER = 3;
+
 export default class Group extends Backbone.Model {
   constructor(attributes, options) {
     super(attributes, options);
 
     this.urlRoot = routes.users;
   }
+
+    isAdmin() {
+        return parseInt(this.get('level'), 10) === ADMIN;
+    }
+
+    isManager() {
+        return parseInt(this.get('level'), 10) === MANAGER;
+    }
+
+    isCollaborator() {
+        return parseInt(this.get('level'), 10) === COLLABORATOR;
+    }
+
+    isViewer() {
+        return parseInt(this.get('level'), 10) === VIEWER;
+    }
 }
 
--- src/views/admin/Users.js
+++ src/views/admin/Users.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
+import 'select2';
 
+import localize from '../../utils/localization';
 import UserCollection from '../../collections/Users';
 import CollectionViewFactory from '../../factories/CollectionViewFactory';
 import ModelViewFactory from '../../factories/ModelViewFactory';
@@ -9,14 +11,33 @@ import bindDialogs from '../../handlers/dialogs';
 const element = 'user';
 const translationKey = 'users';
 
-const ModelView = ModelViewFactory(element, ['name', 'email']);
+const ModelView = ModelViewFactory(element, ['name', 'email', 'level']);
+
+const selectOptions = {
+  width: '100%',
+  minimumResultsForSearch: Infinity,
+};
+
+// FIXME: Don't want this on every page
+$(`#${element}_level`).select2(selectOptions);
 
 class UserView extends ModelView {
   viewData() {
     const data = this.model.toJSON();
 
+    let role = localize.get('users.collaborator');
+
+    if (this.model.isAdmin()) {
+      role = localize.get('users.admin');
+    } else if (this.model.isManager()) {
+      role = localize.get('users.manager');
+    } else if (this.model.isViewer()) {
+      role = localize.get('users.viewer');
+    }
+
     return {
       ...data,
+      role,
       created: dateTimeFormatter(data.created_at),
     };
   }
@@ -25,6 +46,7 @@ class UserView extends ModelView {
 const getInput = () => ({
   name: $(`#${element}_name`).val(),
   email: $(`#${element}_email`).val(),
+  level: parseInt($(`#${element}_level`).val(), 10),
   password: $(`#${element}_password`).val(),
   password_confirmation: $(`#${element}_password_confirmation`).val(),
 });
